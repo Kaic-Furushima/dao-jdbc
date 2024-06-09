3710b475cd5e7ac9725aa238c11566c69646d832
Kaic-Furushima <kaic.fbs@gmail.com>
2024-06-09 01:09:01 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,15 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        Department department = new Department(2, null);
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        Seller seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+
+
     }
 }
