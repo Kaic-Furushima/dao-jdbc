5701bdc71892ee3dbaf9fcc6bf14d4dd827fd854
Kaic-Furushima <kaic.fbs@gmail.com>
2024-06-09 00:57:56 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,14 +5,14 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.List;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        List<Seller> list = sellerDao.findAll();
-        for(Seller obj : list){
-            System.out.println(obj);
-        }
+        Department department = new Department(2, null);
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
